370ba598d6cdec461b9a1396ac57faaa51a1430e
PeaceOloruntoba <profprincepeace@gmail.com>
2025-08-28 20:03:58 +0100

    feat: config

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -3,6 +3,14 @@ import { toast } from "sonner";
 export const handleError = (error: any) => {
   console.error(error);
   const { response } = error;
+  console.log(error);
+  if (error.message === "Network Error") {
+    toast.error("Network Error: Please check your internet connection");
+    return {
+      message: "Network Error: Please check your internet connection",
+      status: 500,
+    };
+  }
   if (response?.data?.message) {
     toast.error(response.data.message);
     return { message: response.data.message, status: response.status };
